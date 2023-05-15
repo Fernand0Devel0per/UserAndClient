06da08da78ec7c89c1e3d8116f4b4d153d553d81
fhlottideveloper <fhlottideveloper@gmail.com>
2023-05-15 19:46:29 -0300

    Update Domains

--- CoreLiveCode/Domain/Client.cs
+++ CoreLiveCode/Domain/Client.cs
@@ -4,6 +4,9 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime AtCreated { get; set; }
         public Guid UserId { get; set; }
     }
 }
