d07ab4aa1971919bb2c13d21224eda23daf923d4
fhlottideveloper <fhlottideveloper@gmail.com>
2023-05-15 21:20:52 -0300

    Withdraw swagger authentication

--- CoreLiveCode/Helpers/Swagger/SwaggerConfiguration.cs
+++ CoreLiveCode/Helpers/Swagger/SwaggerConfiguration.cs
@@ -14,31 +14,6 @@ namespace CoreLiveCode.Helpers.Swagger
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = @"JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.
-                       Example: 'Bearer 12345abcdef'",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                    },
-                    Array.Empty<string>()
-                }
-            });
             });
         }
     }
